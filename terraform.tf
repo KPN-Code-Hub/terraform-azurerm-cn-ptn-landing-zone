caefdf0160e6af02f2061dad62a2351d8a61ae9f
Robin Hendrikse <robin.hendrikse@kpn.com>
2025-07-24 22:57:19 +0200

    Adding landingzone app to argo

--- terraform.tf
+++ terraform.tf
@@ -9,6 +9,9 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
     azapi = {
       source = "Azure/azapi"
     }
@@ -19,6 +22,13 @@ provider "azapi" {
   subscription_id           = "10852416-5fc2-40e3-a117-ae3bc1499f07"
   tenant_id                 = "0baeb517-c6ec-4d6c-a394-96a5affa5ada"
   client_id                 = "ac734034-cf2e-464e-9952-e57fa223a9d6"
+  use_aks_workload_identity = true
+}
+
+provider "azuread" {
+  tenant_id = "0baeb517-c6ec-4d6c-a394-96a5affa5ada"
+  client_id = "ac734034-cf2e-464e-9952-e57fa223a9d6"
+  use_oidc  = true
   use_aks_workload_identity = true
 }
 
