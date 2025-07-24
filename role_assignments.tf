f6d0f33cb36a44987ba2ea22bae24c2fd7866e94
Robin Hendrikse <robin.hendrikse@kpn.com>
2025-07-24 22:28:50 +0200

    Adding landingzone app to argo

--- role_assignments.tf
+++ role_assignments.tf
@@ -1,9 +1,9 @@
-module "role_assignments" {
-  source  = "Azure/avm-res-authorization-roleassignment/azurerm"
-  version = "0.2.0"
-
-  enable_telemetry = false
-
-  role_assignments_azure_resource_manager = local.role_assignments_resource_manager
-  role_assignments_entra_id               = local.role_assignments_entra_id
-}
+#module "role_assignments" {
+#  source  = "Azure/avm-res-authorization-roleassignment/azurerm"
+#  version = "0.2.0"
+#
+#  enable_telemetry = false
+#
+#  role_assignments_azure_resource_manager = local.role_assignments_resource_manager
+#  role_assignments_entra_id               = local.role_assignments_entra_id
+#}
