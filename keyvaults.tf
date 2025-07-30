375ed410130c9b8d99d494c237d90cd8337af8e8
Robin Hendrikse <robin.hendrikse@kpn.com>
2025-07-30 15:30:33 +0200

    Corrected static variable public_network_access_enabled, this one now uses config reference

--- keyvaults.tf
+++ keyvaults.tf
@@ -7,14 +7,14 @@ module "keyvault" {
     for key, value in try(local.keyvaults, {}) : key => value
     if value.enabled == true
   }
-  
+
   name                           = "${each.value.name}-${module.keyvault_naming[each.key].key_vault.name_unique}"
   enable_telemetry               = try(each.value.enable_telemetry, false)
   location                       = module.resource_group[each.value.resource_group_key].resource.location
   resource_group_name            = try(module.resource_group[each.value.resource_group_key].name, each.value.resource_group_name)
   tags                           = local.globals.tags.enabled ? merge(local.globals.tags.object, try(each.value.tags, {})) : try(each.value.tags, {})
   tenant_id                      = try(each.value.tenant_id, data.azurerm_client_config.current.tenant_id)
-  public_network_access_enabled  = false
+  public_network_access_enabled  = try(each.value.public_network_access_enabled, false)
   private_endpoints              = try(each.value.private_endpoints, null)
   legacy_access_policies_enabled = try(each.value.legacy_access_policies_enabled, false)
   legacy_access_policies         = try(each.value.legacy_access_policies_enabled, false) ? try(each.value.legacy_access_policies, null) : null
